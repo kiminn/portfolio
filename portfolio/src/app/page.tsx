db8c7b82bf47d8d0ff2022d6a1600267a1fab739
kiminn <134191815+kiminn@users.noreply.github.com>
2024-04-18 03:42:16 +0900

    scss 설정

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,5 +1,21 @@
+import About from '@/components/About';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Intro from '@/components/Intro';
+import Projects from '@/components/Projects';
+import Skills from '@/components/Skills';
+import Test from '@/components/Test';
+
 export default function Home() {
-  <>
-    
-  </>
+    <>
+        <Header />
+        <main id="main" role="main">
+            <Intro />
+            <Skills />
+            <Projects />
+            <About />
+            <Test />
+        </main>
+        <Footer />
+    </>;
 }
