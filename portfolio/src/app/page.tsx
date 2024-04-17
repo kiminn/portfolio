ded918d02bd78552e62175057d91a0f028ba80a7
kiminn <134191815+kiminn@users.noreply.github.com>
2024-04-18 03:51:32 +0900

    constants, utils

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import About from '@/components/About';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
@@ -5,17 +7,27 @@ import Intro from '@/components/Intro';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 import Test from '@/components/Test';
+import lenis from '@/utils/lenis';
+import link from '@/utils/links';
+import { useEffect } from 'react';
 
 export default function Home() {
-    <>
-        <Header />
-        <main id="main" role="main">
-            <Intro />
-            <Skills />
-            <Projects />
-            <About />
-            <Test />
-        </main>
-        <Footer />
-    </>;
+    useEffect(() => {
+        lenis();
+        link();
+    }, []);
+
+    return (
+        <>
+            <Header />
+            <main id="main" role="main">
+                <Intro />
+                <Skills />
+                <Projects />
+                <About />
+                <Test />
+            </main>
+            <Footer />
+        </>
+    );
 }
